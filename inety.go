49215910a57fb9621146bf1e0ab65b4e654754b4
Ilango Rajagopal <ilangokul@gmail.com>
2017-07-14 13:31:58 +0530

    Pipe code working
    Pipe code cleaned up - working well
    go fmt is used - use for future versions too

--- inety.go
+++ inety.go
@@ -2,30 +2,30 @@
 
 package main
 
-import ( "fmt"
+import (
+	"flag"
+	"fmt"
 	"net"
-	"io"
 	"os"
 	"sync"
-	"flag"
 )
 
 func parseArgs() (addr, port string) {
 
-	var help,h bool
+	var help, h bool
 
-	flag.BoolVar(&help,"help",false,"Show usage")
-	flag.BoolVar(&h,"h",false,"Show usage")
-	flag.StringVar(&port,"p","","Port number")
-	flag.StringVar(&addr,"a","","Address to connect to")
+	flag.BoolVar(&help, "help", false, "Show usage")
+	flag.BoolVar(&h, "h", false, "Show usage")
+	flag.StringVar(&port, "p", "", "Port number")
+	flag.StringVar(&addr, "a", "", "Address to connect to")
 	flag.Parse()
 
-	if (help || h) {
+	if help || h {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	if (  (addr == "" || port == "") && flag.NArg() > 0) {
+	if (addr == "" || port == "") && flag.NArg() > 0 {
 		switch flag.NArg() {
 		default:
 			fallthrough
@@ -36,43 +36,53 @@ func parseArgs() (addr, port string) {
 			addr = flag.Arg(0)
 		}
 	}
-	return;
+	return
 
 }
 
 func main() {
-	var addr,port string = parseArgs()
+	var addr, port string = parseArgs()
 	var wg sync.WaitGroup
 
-	if(addr == "" || port == "") {
+	if addr == "" || port == "" {
 		fmt.Println("Enter addr,port")
-		fmt.Scan(&addr,&port)
+		fmt.Scan(&addr, &port)
 	}
 
-	conn, err := net.Dial("tcp",net.JoinHostPort(addr,port))
+	conn, err := net.Dial("tcp", net.JoinHostPort(addr, port))
 	defer conn.Close()
 
 	if err != nil {
-		fmt.Println("Error while connecting...",err)
+		fmt.Println("Error while connecting...", err)
 	}
 
 	wg.Add(2)
 
 	go func() {
 		defer wg.Done()
-		for _,err = io.Copy(os.Stdout,conn) ; err == nil; _,err = io.Copy(os.Stdout,conn) {
-			//nothing to do
+		buf := make([]byte, 512, 512)
+		for nr, err := conn.Read(buf); err == nil; nr, err = conn.Read(buf) {
+			nw, err := os.Stdout.Write(buf[:nr])
+			if nr != nw && err != nil {
+				break
+			}
 		}
 	}()
 
 	go func() {
 		defer wg.Done()
-		for _,err = io.Copy(conn,os.Stdin) ; err ==nil; _,err = io.Copy(conn,os.Stdin) {
-			//nothing to do actually
+		buf := make([]byte, 512, 512)
+		for nr, err := os.Stdin.Read(buf); err == nil; nr, err = os.Stdin.Read(buf) {
+			nw, err := conn.Write(buf[:nr])
+			if nr != nw && err != nil {
+				break
+			}
 		}
 	}()
 
-	if err != nil { fmt.Println("Error: ",err) }
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 
 	wg.Wait()
 }
--- inety_debug.go
+++ inety_debug.go
@@ -2,30 +2,30 @@
 
 package main
 
-import ( "fmt"
+import (
+	"flag"
+	"fmt"
 	"net"
-	"io"
 	"os"
 	"sync"
-	"flag"
 )
 
 func parseArgs() (addr, port string) {
 
-	var help,h bool
+	var help, h bool
 
-	flag.BoolVar(&help,"help",false,"Show usage")
-	flag.BoolVar(&h,"h",false,"Show usage")
-	flag.StringVar(&port,"p","","Port number")
-	flag.StringVar(&addr,"a","","Address to connect to")
+	flag.BoolVar(&help, "help", false, "Show usage")
+	flag.BoolVar(&h, "h", false, "Show usage")
+	flag.StringVar(&port, "p", "", "Port number")
+	flag.StringVar(&addr, "a", "", "Address to connect to")
 	flag.Parse()
 
-	if (help || h) {
+	if help || h {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	if (  (addr == "" || port == "") && flag.NArg() > 0) {
+	if (addr == "" || port == "") && flag.NArg() > 0 {
 		switch flag.NArg() {
 		default:
 			fallthrough
@@ -36,25 +36,25 @@ func parseArgs() (addr, port string) {
 			addr = flag.Arg(0)
 		}
 	}
-	return;
+	return
 
 }
 
 func main() {
-	var addr,port string = parseArgs()
+	var addr, port string = parseArgs()
 	var wg sync.WaitGroup
 
-	if(addr == "" || port == "") {
+	if addr == "" || port == "" {
 		fmt.Println("Enter addr,port")
-		fmt.Scan(&addr,&port)
-		fmt.Println("Your input addr: ",addr," port: ",port)
+		fmt.Scan(&addr, &port)
+		fmt.Println("Your input addr: ", addr, " port: ", port)
 	}
 
-	conn, err := net.Dial("tcp",net.JoinHostPort(addr,port))
+	conn, err := net.Dial("tcp", net.JoinHostPort(addr, port))
 	defer conn.Close()
 
 	if err != nil {
-		fmt.Println("Error while connecting...",err)
+		fmt.Println("Error while connecting...", err)
 	}
 	fmt.Println("connected")
 
@@ -62,21 +62,31 @@ func main() {
 
 	go func() {
 		defer wg.Done()
-		for _,err = io.Copy(os.Stdout,conn) ; err == nil; _,err = io.Copy(os.Stdout,conn) {
-			//nothing to do
+		buf := make([]byte, 512, 512)
+		for nr, err := conn.Read(buf); err == nil; nr, err = conn.Read(buf) {
+			nw, err := os.Stdout.Write(buf[:nr])
+			if nr != nw && err != nil {
+				break
+			}
 		}
-		fmt.Println("Output is over :",err)
+		fmt.Println("Output is over :", err)
 	}()
 
 	go func() {
 		defer wg.Done()
-		for _,err = io.Copy(conn,os.Stdin) ; err ==nil; _,err = io.Copy(conn,os.Stdin) {
-			//nothing to do actually
+		buf := make([]byte, 512, 512)
+		for nr, err := os.Stdin.Read(buf); err == nil; nr, err = os.Stdin.Read(buf) {
+			nw, err := conn.Write(buf[:nr])
+			if nr != nw && err != nil {
+				break
+			}
 		}
-		fmt.Println("Input is over: ",err)
+		fmt.Println("Input is over: ", err)
 	}()
 
-	if err != nil { fmt.Println("Error: ",err) }
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 
 	wg.Wait()
 }
